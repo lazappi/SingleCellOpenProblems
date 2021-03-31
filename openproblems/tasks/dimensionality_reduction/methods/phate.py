b260d6bab0d90e0f8cbbce23e754c29851136d9e
Luke Zappia <lazappi@users.noreply.github.com>
2021-03-31 15:16:52 +0200

    Fix formatting for Black

--- openproblems/tasks/dimensionality_reduction/methods/phate.py
+++ openproblems/tasks/dimensionality_reduction/methods/phate.py
@@ -11,6 +11,7 @@ def _phate(adata):
     adata.obsm["X_emb"] = phate_op.fit_transform(adata.X)
     return adata
 
+
 @method(
     method_name="PHATE (default pre-processing)",
     paper_name="Visualizing Transitions and Structure for Biological Data Exploration",
@@ -23,6 +24,7 @@ def _phate(adata):
 def phate_default(adata):
     sqrt_cpm(adata)
     return _phate(adata)
+
 
 @method(
     method_name="PHATE (scanpy pre-processing)",
